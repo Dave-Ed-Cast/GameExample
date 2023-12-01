dc4f5a9e9c9340b147a4dc97d73b340cdd2430df
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2023-12-01 15:58:40 +0100

    First push

---
+++ SpaceShooter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpaceShooter
+//
+//  Created by Davide Castaldi on 01/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpaceShooter/SpaceShooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceShooterApp.swift
+//  SpaceShooter
+//
+//  Created by Davide Castaldi on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceShooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
